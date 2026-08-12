9fc68feeaed3baa2a76d6df3d11ae3099bfc1e92
jeoungho.yoo <edel1212@naver.com>
2026-08-12 17:27:34 +0900

    fail PR CI

--- src/test/java/com/sample/test/TestApplicationTests.java
+++ src/test/java/com/sample/test/TestApplicationTests.java
@@ -1,13 +1,19 @@
 package com.sample.test;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
+        log.info("Hi");
+        assertEquals(1,4);
 	}
 
 }
