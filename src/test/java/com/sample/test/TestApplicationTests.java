f8dbd3a936c06cd6e3a54983bdb7724d9224afa5
jeoungho.yoo <edel1212@naver.com>
2026-08-13 11:40:34 +0900

    fail test

--- src/test/java/com/sample/test/TestApplicationTests.java
+++ src/test/java/com/sample/test/TestApplicationTests.java
@@ -3,11 +3,14 @@ package com.sample.test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
+        assertEquals(1,3);
 	}
 
 }
